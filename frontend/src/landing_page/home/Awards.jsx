e025cf53a8ecbc8e279044d4e2c53ead5562b889
sairam <mr.sairamofficial@gmail.com>
2025-08-15 13:59:15 +0530

    Building home page

--- frontend/src/landing_page/home/Awards.jsx
+++ frontend/src/landing_page/home/Awards.jsx
@@ -1,5 +1,49 @@
-export default function Awards(){
+export default function Awards() {
     return (
-        <h1>Awards</h1>
+        <div className="container py-5">
+            <div className="row align-items-center">
+                
+                <div className="col-md-6 p-5">
+                    <img 
+                        src="media/images/largestBroker.svg" 
+                        alt="Largest broker" 
+                        className="img-fluid"
+                    />
+                </div>
+
+                <div className="col-md-6">
+                    <h1 className="mb-4">Largest Stock Broker In India</h1>
+                    
+                    <p className="mb-4">
+                        2+ million Zerodha clients contribute to over 15% of all retail order volumes 
+                        in India daily by trading and investing in:
+                    </p>
+
+                    <div className="row mb-4">
+                        <div className="col-6">
+                            <ul>
+                                <li className="mb-2">Futures and Options</li>
+                                <li className="mb-2">Commodity Derivatives</li>
+                                <li className="mb-2">Currency Derivatives</li>
+                            </ul>
+                        </div>
+                        <div className="col-6">
+                            <ul>
+                                <li className="mb-2">Stocks and IPOs</li>
+                                <li className="mb-2">Direct Mutual Funds</li>
+                                <li className="mb-2">Bonds & Govt. Securities</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <img 
+                        src="media/images/pressLogos.png" 
+                        alt="Press Logos" 
+                        className="img-fluid"
+                        style={{width:"90%"}}
+                    />
+                </div>
+            </div>
+        </div>
     );
 }
--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -1,5 +1,12 @@
-export default function Hero(){
+export default function Hero() {
     return (
-        <h1>Hero</h1>
+        <div className="container p-5 mb-5">
+            <div className="row text-center">
+                <img src="media/images/homeHero.png" alt="hero image" className="mb-5" />
+                <h1 className="mt-5">Invest In Everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button className="p-2 btn btn-primary fs-5 mb-5" style={{ width: "20%", margin: "0 auto", }}>Sign Up for Free</button>
+            </div>
+        </div>
     )
 }
