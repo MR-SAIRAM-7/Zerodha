d49a014e8aff8198e32e6415ce8081e8e1b419c4
sairam <mr.sairamofficial@gmail.com>
2025-08-16 13:21:09 +0530

    creating navbar and footer, home section completed

--- frontend/src/landing_page/Footer.jsx
+++ frontend/src/landing_page/Footer.jsx
@@ -1,5 +1,81 @@
-export default function Footer(){
+import "./Footer.css"
+export default function Footer() {
     return (
-        <h1>Footer</h1>
+        <footer className="bg-light text-center text-md-start py-4 border-top">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                        <img src="media/images/logo.svg" alt="Zerodha" className="mb-3" style={{ width: "50%" }} />
+                        <p className="text-muted small">&copy; 2010 - 2025, Zerodha Broking Ltd.</p>
+                    </div>
+                    <div className="col-md-2">
+                        <h6 className="text-uppercase fw-bold">Account</h6>
+                        <ul className="list-unstyled">
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Open demat account</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Sign up</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-2">
+                        <h6 className="text-uppercase fw-bold">Support</h6>
+                        <ul className="list-unstyled">
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Contact us</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >complaints</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Bulletin</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Circular</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Z-Connect blog</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Downloads</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-2">
+                        <h6 className="text-uppercase fw-bold">Company</h6>
+                        <ul className="list-unstyled">
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >About</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Products</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Pricing</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Careers</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Zerodha Cares (CSR)</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Zerodha.tech</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Open source</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-3">
+                        <h6 className="text-uppercase fw-bold">Quick links</h6>
+                        <ul className="list-unstyled">
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Upcoming IPOs</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Economic calendar</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Calculators</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Markets</a></li>
+                            <li><a href="#" className="text-muted text-decoration-none link-hover" >Sectors</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <hr className="my-4" />
+                <div className="row">
+                    <div className="col-12 text-muted small">
+                        <p>Zerodha Broking Ltd.: Member of NSE, BSE & MCX - SEBI Registration no.: INZ000031833 CDSL/NSDL: Depository services through Zerodha Broking Ltd. - SEBI Registration no.: IN-DP-431-2019 Commodity Trading through Zerodha Commodities Pvt. Ltd. MCX: 46025; SEBI Registration no.: INZ000038238 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@zerodha.com or for DP related to dp@zerodha.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF</p>
+                        <p>Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, e-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances</p>
+                        <p>Smart Online Dispute Resolution | Grievances Redressal Mechanism</p>
+                        <p>Investments in securities market are subject to market risks, read all the related documents carefully before investing.</p>
+                        <p>Attention investors: 1) Stock brokers can accept securities as margin from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.</p>
+                        <p>India's largest broker based on networth as per NSE, BSE broker factsheet</p>
+                        <p>"Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.</p>
+                        <p className="mt-3">
+                            <a href="#" className="text-muted text-decoration-none link-hover me-3">NSE</a>
+                            <a href="#" className="text-muted text-decoration-none link-hover me-3">BSE</a>
+                            <a href="#" className="text-muted text-decoration-none link-hover me-3">MCX</a>
+                            <a href="#" className="text-muted text-decoration-none link-hover me-3">Terms & conditions</a>
+                            <a href="#" className="text-muted text-decoration-none link-hover me-3">Policies & procedures</a>
+                            <a href="#" className="text-muted text-decoration-none link-hover me-3">Privacy policy</a>
+                            <a href="#" className="text-muted text-decoration-none link-hover me-3">Disclosure</a>
+                            <a href="#" className="text-muted text-decoration-none link-hover">Investor charter</a>
+                        </p>
+                    </div>
+                </div>
+                <div className="text-center mt-3">
+                    <p className="text-dark fw-bold">Developed by SaiRam</p>
+                </div>
+            </div>
+        </footer>
     )
 }
+
--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,5 +1,46 @@
-export default function Navbar(){
+export default function Navbar() {
     return (
-        <h1>Navbar</h1>
-    )
+        <nav className="navbar navbar-expand-lg border-bottom p-3" style={{ backgroundColor: "white" }}>
+            <div className="container-fluid">
+                {/* Logo with left margin */}
+                <a className="navbar-brand ms-3" href="#">
+                    <img src="media/images/logo.svg" style={{ width: "25%" }} alt="Logo" />
+                </a>
+
+                {/* Toggler for mobile */}
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                {/* Nav Items */}
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item mx-3">
+                            <a className="nav-link active" aria-current="page" href="#">SignUp</a>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link active" href="#">About</a>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link active" href="#">Products</a>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link active" href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item mx-3">
+                            <a className="nav-link active" href="#">Support</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
