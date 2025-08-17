1eee2a01b305adbfaf4c1e1c902e6d8a18983b53
sairam <mr.sairamofficial@gmail.com>
2025-08-17 12:47:13 +0530

    productpage changes

--- frontend/src/landing_page/products/Hero.jsx
+++ frontend/src/landing_page/products/Hero.jsx
@@ -1,5 +1,11 @@
-export default function Hero(){
+export default function Hero() {
     return (
-        <h1>Hero</h1>
+        <div className="container">
+            <div className="row p-5 mt-4">
+                <h1 className="fs-1 text-center text-muted">Technology</h1>
+                <p className="fs-4 text-center text-muted">Sleek, modern, and intuitive trading platforms</p>
+                <p className="text-center">Checkout our  <a href="#" style={{ textDecoration: "none" }}>Investment Oppurtunites<i className="fa-solid fa-arrow-right"></i></a></p>
+            </div>
+        </div>
     )
 }
